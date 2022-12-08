12086936168e91978469aa055c14ec38290dcc21
vigneshwaranselvarajan <vickymech.engg20@gmail.com>
2022-12-08 23:45:03 +0530

    vicky code

--- src/test/java/org/emp/PracticeClass.java
+++ src/test/java/org/emp/PracticeClass.java
@@ -11,5 +11,14 @@ public class PracticeClass {
 	public void method3() {
 		System.out.println("3");
 	}
+	public void method7() {
+		System.out.println("7");
+	}
+	public void method8() {
+		System.out.println("8");
+	}
+	public void method9() {
+		System.out.println("9");
+	}
 	
 }
