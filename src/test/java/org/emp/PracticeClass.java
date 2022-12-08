377f41e1c565ebf3fa2ff666e17af24aac270586
vigneshwaranselvarajan <vickymech.engg20@gmail.com>
2022-12-08 19:29:25 +0530

    initial code

---
+++ src/test/java/org/emp/PracticeClass.java
@@ -0,0 +1,15 @@
+package org.emp;
+
+public class PracticeClass {
+	
+	public void method1() {
+		System.out.println("1");
+	}
+	public void method2() {
+		System.out.println("2");
+	}
+	public void method3() {
+		System.out.println("3");
+	}
+	
+}
